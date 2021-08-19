dee988250f46a672a5380e011dc41c274fd53ba9
Andrey_Del_Bello <andrey.delbello2002@gmail.com>
2021-08-19 17:54:35 +0000

    Atividade Balanco Trimestral

--- Balancotrimestral.java
+++ Balancotrimestral.java
@@ -1,3 +1,11 @@
 public class Balancotrimestral{
-    
+    public static void main(String[] args){
+        int gastosJaneiro = 15000;
+        int gastosFevereiro = 23000;
+        int gastosMarco = 17000;
+        int gastosTrimestre = (gastosJaneiro + gastosFevereiro + gastosMarco);
+        System.out.println("Gastos do trimestre: "+ gastosTrimestre);
+        int mediatotalmensal= gastosTrimestre/3;
+        System.out.println("Média mensal: "+ mediatotalmensal);
+    }
 }
